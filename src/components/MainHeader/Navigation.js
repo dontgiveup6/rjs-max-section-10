a29ba3b801663a8b61642b480c60ca6f4af41bbd
Georgi Serafimov <g.serafimov@c-trust.eu>
2022-10-18 17:58:54 +0300

    continue

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../context/auth-context";
 
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const ctxData = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctxData) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctxData.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctxData.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctxData.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctxData.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctxData.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctxData.isLoggedIn && (
+          <li>
+            <button onClick={ctxData.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
